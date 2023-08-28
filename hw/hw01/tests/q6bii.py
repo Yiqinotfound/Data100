8ce4f71f28b706bd17070c0c397a27aaa4f2debf
杨弋奇 <1214771220@qq.com>
2023-08-28 19:42:24 +0800

    hw1

--- hw/hw01/tests/q6bii.py
+++ hw/hw01/tests/q6bii.py
@@ -1,6 +1,2 @@
 OK_FORMAT = True
-OK_FORMAT = True
-OK_FORMAT = True
-OK_FORMAT = True
-OK_FORMAT = True
-test = {'name': 'q6bii', 'points': None, 'suites': [{'cases': [{'code': '>>> 0 <= q6bii_r <= 1\n', 'hidden': False, 'locked': False}], 'scored': True, 'setup': '', 'teardown': '', 'type': 'doctest'}]}
+test = {'name': 'q6bii', 'points': None, 'suites': [{'cases': [{'code': '>>> 0 <= q6bii_r <= 1\n', 'hidden': True, 'locked': False}], 'scored': True, 'setup': '', 'teardown': '', 'type': 'doctest'}]}
